ead094679c8cb836d33845e96a7cdb61833af4ee
matukai <matt.tanji@gmail.com>
2018-03-15 20:33:31 -1000

    weather data being stored on global variable

--- server/server.js
+++ server/server.js
@@ -20,35 +20,62 @@ app.use('/trails', trails);
 app.use('/weather', weather);
 app.use(express.static('pubic'));
 
-//IN MEMORY
-let inMemTrails = {}
-
-app.get('/', (req,res) => {
-  console.log('i got fired')
-  return new Trail ()
-  .fetchAll()
-  .then(result => {
-    result.map(element => {
-      inMemTrails[element.attributes.trailname] = {
-        coordinates: element.attributes.coordinates[0]}
-    })
-    weatherData(inMemTrails)
-  })
-  .catch(err => {
-    return res.json({message: err.message})
-  })
-})
+global.hikeNowWeather = {
+  station_id: '',
+  weather: '',
+  temperature_string: '',
+  relative_humidity: '',
+  wind_dir: '',
+  wind_mph: '',
+  wind_gust_mph: '',
+  feelslike_string: '',
+  visibility_mi: '',
+  precip_1hr_string: '',
+  precip_today_in: '',
+  observation_location_latitude: '',
+  observation_location_longitude: '',
+}
 
 let WEATHERAPIKEY = weatherKey.weather.apiKey;
 
-function weatherData (obj) {
-  for(var i in obj){
-    let latitude = obj[i].coordinates[1]
-    let longitude = obj[i].coordinates[0]
-    rp(`http://api.wunderground.com/api/${WEATHERAPIKEY}/conditions/q/${latitude},${longitude}.json`, (err,response, body) => {
-      console.log(' yo its the weather',body)
+function weatherData () {
+  let trails = {}
+  new Trail()
+  .fetchAll()
+  .then(result => {
+    result.map(element => {
+      trails[element.attributes.trailname] = {
+      coordinates: element.attributes.coordinates[0]}
     })
-  }
+  })
+  rp(`http://api.wunderground.com/api/d32b00d3f891bff3/conditions/q/21.5633764486087,-158.237758592625.json`, (err,response, body) => {
+        let data = JSON.parse(body)
+        global.hikeNowWeather = {
+          station_id: data.current_observation.station_id,
+          weather: data.current_observation.weather,
+          temperature_string: data.current_observation.temperature_string,
+          relative_humidity: data.current_observation.relative_humidity,
+          wind_dir: data.current_observation.wind_dir,
+          wind_mph: data.current_observation.wind_mph,
+          wind_gust_mph: data.current_observation.wind_gust_mph,
+          feelslike_string: data.current_observation.feelslike_string,
+          visibility_mi: data.current_observation.visibility_mi,
+          precip_1hr_string: data.current_observation.precip_1hr_string,
+          precip_today_in: data.current_observation.precip_today_in,
+          observation_location_latitude: data.current_observation.observation_location.latitude,
+          observation_location_longitude: data.current_observation.observation_location.longitude,
+        }
+  // for(var i in trails){
+  //   let latitude = trails[i].coordinates[1]
+  //   let longitude = trails[i].coordinates[0]
+  //   rp(`http://api.wunderground.com/api/d32b00d3f891bff3/conditions/q/${latitude},${longitude}.json`, (err,response, body) => {
+  //     let data = JSON.parse(body)
+  //     if(data.current_observation.temp_f){
+  //       return obj[i].temp_f = data.current_observation.temp_f
+  //     }else if(data.current_observation.temp_f == undefined){
+  //       return obj[i].temp_f = 'unknown'
+  //     }
+  })
 }
 
 
@@ -60,4 +87,5 @@ function weatherData (obj) {
 
 app.listen(PORT, () => {
   console.log(`SERVER IS LISTENING ON ${PORT}`)
+  weatherData()
 });
