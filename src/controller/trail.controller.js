29e173b7b24f45e1ed43fb32f244cdbb84c576b1
nick <nicnhines3@gmail.com>
2018-03-17 19:28:51 -1000

    switching branches

--- src/controller/trail.controller.js
+++ src/controller/trail.controller.js
@@ -1,8 +1,11 @@
-
-const TrailCtrl = [`$scope`, `trailService`, function ($scope, trailService) {
+const MAPKEY = require('../../config/config');
+const TrailCtrl = [`$scope`, `trailService`,`googleMap`,
+function ($scope, trailService, googleMap) {
   $scope.trail = 'TRAIL TAILS'
   
-  trailService.getTrailShit()
+  trailService.getTrailShit();
+  $scope.googleMap=`https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyBVGDEMV8YdofiqyfodxHRVYg9UQZjzbvo
+  `
 }]
 
 export default TrailCtrl
