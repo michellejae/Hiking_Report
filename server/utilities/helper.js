2df1c29dee3b8e3250dba41de1fed38fd59c3daf
nick <nicnhines3@gmail.com>
2018-03-17 16:05:12 -1000

    cleaned more code

--- server/utilities/helper.js
+++ server/utilities/helper.js
@@ -32,7 +32,7 @@ global.hikeNow.weather = {
 };
 
 function timedCalls() {
-  cron.scheduleJob({ rule:' 0 0 6,9,12,15 * * *'},  
+  cron.scheduleJob({ rule:' 0 0 6,9,12,15 * * *'},
   function getTrailHeads() {   
     let trails = [];
     new Trail()
@@ -41,10 +41,10 @@ function timedCalls() {
       result.map(element => {
         trails.push(element.attributes.coordinates[0]);
       })      
-     fireWeatherAPI(/*trails*/);
+     fireWeatherAPI(trails);
     })
-    }
-  )
+    console.log('still working')
+  })
 };
 
 function fireWeatherAPI (arr) {
@@ -57,41 +57,41 @@ function fireWeatherAPI (arr) {
 };
 
 function getWeatherData(lat,long){
-    return rp(`${WEATHER_API_ENDPOINT}${lat},${long}.json`)
-      .then(json => {
-        return JSON.parse(json);
-      })
-      .then(data => {
-        if(data.current_observation.station_id){
-          global.hikeNow[data.current_observation.station_id] = {
-            city: data.current_observation.display_location.city,
-            state: data.current_observation.display_location.city,
-            longitude: data.current_observation.display_location.longitude,
-            latitude: data.current_observation.display_location.latitude,
-            observation_time: data.current_observation.observation_time,
-            weather: data.current_observation.weather,
-            temp_f: data.current_observation.temp_f,
-            temp_c: data.current_observation.temp_c,
-            relative_humidity: data.current_observation.relative_humidity,
-            wind_dir: data.current_observation.wind_dir,
-            wind_degrees: data.current_observation.wind_degrees,
-            wind_mph: data.current_observation.wind_mph,
-            wind_gust_mph: data.current_observation.wind_gust_mph,
-            wind_gust_kph: data.current_observation.wind_gust_kph,
-            feelslike_f: data.current_observation.feelslike_f,
-            feelslike_c: data.current_observation.feelslike_c,
-            visibility_mi: data.current_observation.visibility_mi,
-            visibility_km: data.current_observation.visibility_km,
-            UV: data.current_observation.UV
-          }
-        }else{
-          return global.hikeNow.weather;
-        }
-        //console.log('GLOBAL VARIABLE hikeNow ',global.hikeNow)
-      })
-      .catch(err => {
-        console.log(err)
-      });
+  return rp(`${WEATHER_API_ENDPOINT}${lat},${long}.json`)
+  .then(json => {
+    return JSON.parse(json);
+  })
+  .then(data => {
+    if(data.current_observation.station_id){
+      global.hikeNow[data.current_observation.station_id] = {
+        city: data.current_observation.display_location.city,
+        state: data.current_observation.display_location.city,
+        longitude: data.current_observation.display_location.longitude,
+        latitude: data.current_observation.display_location.latitude,
+        observation_time: data.current_observation.observation_time,
+        weather: data.current_observation.weather,
+        temp_f: data.current_observation.temp_f,
+        temp_c: data.current_observation.temp_c,
+        relative_humidity: data.current_observation.relative_humidity,
+        wind_dir: data.current_observation.wind_dir,
+        wind_degrees: data.current_observation.wind_degrees,
+        wind_mph: data.current_observation.wind_mph,
+        wind_gust_mph: data.current_observation.wind_gust_mph,
+        wind_gust_kph: data.current_observation.wind_gust_kph,
+        feelslike_f: data.current_observation.feelslike_f,
+        feelslike_c: data.current_observation.feelslike_c,
+        visibility_mi: data.current_observation.visibility_mi,
+        visibility_km: data.current_observation.visibility_km,
+        UV: data.current_observation.UV
+      }
+    }else{
+      return global.hikeNow.weather;
+    }
+    //console.log('GLOBAL VARIABLE hikeNow ',global.hikeNow)
+  })
+  .catch(err => {
+    console.log(err)
+  });
 };
 
 // app.get('/api/hikeNow/fake', (req,res) => {
