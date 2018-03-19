76bf6b102716e106763359e6f9bf1dcddd7dd100
nick <nicnhines3@gmail.com>
2018-03-19 10:02:17 -1000

    trying new method for map in other branch

--- src/controller/trail.controller.js
+++ src/controller/trail.controller.js
@@ -1,10 +1,10 @@
 const MAPKEY = require('../../config/config');
-const TrailCtrl = [`$scope`, `trailService`,`googleMap`,
-function ($scope, trailService, googleMap) {
-  $scope.trail = 'TRAIL TAILS'
+const TrailCtrl = [`$scope`, `trailService`,`googleMapsUrl`,
+function ($scope, $http, trailService, googleMapsUrl) {
+  //$scope.trail = 'TRAIL TAILS'
   
-  trailService.getTrailShit();
-  $scope.googleMap=`https://maps.googleapis.com/maps/api/js?key=AIzaSyBEgr87u1-3vCotB7dgKNnGv9Q5qusIBt8&callback=initMap`;
+  //trailService.getTrailShit();
+  $scope.googleMapsUrl=`https://maps.googleapis.com/maps/api/js?key=AIzaSyBEgr87u1-3vCotB7dgKNnGv9Q5qusIBt8&callback=initMap`;
 }]
 
 export default TrailCtrl
