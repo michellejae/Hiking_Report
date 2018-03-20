42b2614242910af29bbebc834e20b64a72fb5bf9
nick <nicnhines3@gmail.com>
2018-03-19 17:31:06 -1000

    working map

--- src/controller/trail.controller.js
+++ src/controller/trail.controller.js
@@ -1,14 +1,14 @@
 const MAPKEY = require('../../config/config');
-const TrailCtrl = [`$scope`, `trailService`,`googleMapsUrl`,
-function ($scope, $http, trailService, googleMapsUrl) {
-    NgMap.getMap().then(function(map) {
-      console.log(map.getCenter());
-      console.log('markers', map.markers);
-      console.log('shapes', map.shapes);
-    })
+const TrailCtrl = [`$scope`, `trailService`,`NgMap`,
+function ($scope, $http, trailService, NgMap) {
+    // NgMap.getMap().then(function(map) {
+    //   console.log(map.getCenter());
+    //   console.log('markers', map.markers);
+    //   console.log('shapes', map.shapes);
+    // })
   $scope.trail = 'TRAIL TAILS'
   
-  trailService.getTrailShit();
+  // trailService.getTrailShit();
   $scope.googleMapsUrl=`https://maps.googleapis.com/maps/api/js?key=AIzaSyBEgr87u1-3vCotB7dgKNnGv9Q5qusIBt8&callback=initMap`;
 }]
 
