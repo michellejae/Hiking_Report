fa13285d416e64ed214607557e958c770888945a
nick <nicnhines3@gmail.com>
2018-03-19 19:16:30 -1000

    took out unneeded css

--- src/controller/trail.controller.js
+++ src/controller/trail.controller.js
@@ -1,15 +1,11 @@
 const MAPKEY = require('../../config/config');
 const TrailCtrl = [`$scope`, `trailService`,`NgMap`,
 function ($scope, $http, trailService, NgMap) {
-    // NgMap.getMap().then(function(map) {
-    //   console.log(map.getCenter());
-    //   console.log('markers', map.markers);
-    //   console.log('shapes', map.shapes);
-    // })
+
   $scope.trail = 'TRAIL TAILS'
   
   // trailService.getTrailShit();
-  $scope.googleMapsUrl=`https://maps.googleapis.com/maps/api/js?key=AIzaSyBEgr87u1-3vCotB7dgKNnGv9Q5qusIBt8&callback=initMap`;
+  $scope.googleMapsUrl=`https://maps.googleapis.com/maps/api/js?key=${MAPKEY.map.key}&callback=initMap`;
 }]
 
 export default TrailCtrl
