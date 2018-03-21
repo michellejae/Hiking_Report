4cd751e2d7fd4a337a14e04e295c0f0699bbeac1
matukai <matt.tanji@gmail.com>
2018-03-21 09:24:40 -1000

    cleaning before pr

--- src/controller/allTrails.controller.js
+++ src/controller/allTrails.controller.js
@@ -1,9 +1,14 @@
 const MAPKEY = require('../../config/config');
-const AllTrailsCtrl = ['$scope','allTrailsService', 'NgMap', function ($scope, allTrailsService, NgMap){
+const AllTrailsCtrl = ['$scope','$routeParams','allTrailsService', 'NgMap', function ($scope,$routeParams, allTrailsService, NgMap){
+
+  let name = $routeParams.name
+  console.log(name)
+
   allTrailsService.fetchAllTrails();
   $scope.trails = allTrailsService.getAllTrails();
   $scope.trailHeads = allTrailsService.getTrailHeadCoordinates();
   $scope.googleMapsUrl=`https://maps.googleapis.com/maps/api/js?key=${MAPKEY.map.key}&callback=initMap`;
+
 }];
 
 export default AllTrailsCtrl;
