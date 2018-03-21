05f8810a62ff6b82d0b6d914c147944cf9c42833
matukai <matt.tanji@gmail.com>
2018-03-20 21:06:53 -1000

    cleaning before merging

--- server/server.js
+++ server/server.js
@@ -111,7 +111,7 @@ app.get('/api/hikeNow/all', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`SERVER IS LISTENING ON ${PORT}`);
-  getTrailHeads()
+  // getTrailHeads()
   // timedCalls(); 
   // updateWeatherStations();
   // getRainData();
