5f89d71872ade87c653bb25dbbc6da844615bbc6
matukai <matt.tanji@gmail.com>
2018-03-21 12:07:41 -1000

    marker chosen displays name... going to work on getting status to display

--- src/controller/allTrails.controller.js
+++ src/controller/allTrails.controller.js
@@ -5,13 +5,10 @@ const AllTrailsCtrl = ['$scope','allTrailsService', 'NgMap', function ($scope, a
   $scope.trailHeads = allTrailsService.getTrailHeadCoordinates();
 
   $scope.showInfo = function (event,name) {
-    console.log('fire name ',name)
     $scope.selectedHike = $scope.trailHeads.filter(function(c){
       return c.trailname === name;
     })[0];
   };
-
-
 
   $scope.googleMapsUrl=`https://maps.googleapis.com/maps/api/js?key=${MAPKEY.map.key}&callback=initMap`;
 
