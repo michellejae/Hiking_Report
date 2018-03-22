5e6b5ef8113e044062b2d0d9b77750b1aa77f66c
matukai <matt.tanji@gmail.com>
2018-03-21 15:32:43 -1000

    ng-if rendering working

--- src/controller/allTrails.controller.js
+++ src/controller/allTrails.controller.js
@@ -1,6 +1,5 @@
 const MAPKEY = require('../../config/config');
 const AllTrailsCtrl = ['$scope','allTrailsService', 'NgMap', function ($scope, allTrailsService, NgMap){
-  let b = false;
 
   //Load All Trails
   allTrailsService.fetchAllTrails();
@@ -11,17 +10,19 @@ const AllTrailsCtrl = ['$scope','allTrailsService', 'NgMap', function ($scope, a
   //Set trailHeads to scope var to plot markers
   $scope.trailHeads = allTrailsService.getTrailHeadCoordinates();
 
-  //Display selected trail info or not to display 
-  function isSelected () {
-    return b = true;
+  //Display or not to display
+  let b = false;
+  $scope.selected = function () {
+    return b;
   }
 
   //Set selectedHike to scope to display selected marker
   $scope.showInfo = function (event,name) {
-    isSelected();
     $scope.selectedHike = $scope.trailHeads.filter(function(c){
       return c.trailname === name;
     })[0];
+    b = true;
+    $scope.selected()
   };
 
   //Google Map
